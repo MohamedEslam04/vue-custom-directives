d56bde1f051f7afbb37159d99aea9a85696107ab
Mohamed Eslam <60846785+MohamedEslam04@users.noreply.github.com>
2025-06-09 21:32:09 +0300

    chore: update package dependencies and improve GitHub Actions workflow for testing and publishing

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [vue() as any],
+  plugins: [vue()],
   test: {
     environment: 'happy-dom',
     coverage: {
